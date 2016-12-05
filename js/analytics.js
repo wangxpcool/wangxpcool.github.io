55c79ff25ade6518792735593c3dfde9d0d2f101
wangxpcool <wangxpcool@qq.com>
2016-12-05 21:07:40 +0800

    Create analytics.js

---
+++ js/analytics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?934ef2122154aa36fa642839d9867c4c";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
